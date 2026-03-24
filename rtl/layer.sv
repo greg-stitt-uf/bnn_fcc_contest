3365b25436e584b758d74e764ec457a29e57deb6
jackie-giyu <wjackiemath@gmail.com>
2026-03-24 19:54:03 -0400

    Started 3 layer bnn and modified layer

--- rtl/layer.sv
+++ rtl/layer.sv
@@ -1,23 +1,24 @@
 module layer #(
-    parameter BEAT_WIDTH = 16,
-    parameter NUM_INPUTS = 784,
-    parameter NUM_NEURONS = 256,
-    parameter NUM_WEIGHTS = NUM_INPUTS,
-    parameter int POPCOUNT_WIDTH = 32,
-    parameter int NUM_BEATS = $ceil(NUM_INPUTS / BEAT_WIDTH) // 784/16 = 49 beats
+    parameter int BEAT_WIDTH = 16,
+    parameter int NUM_INPUTS = 784,
+    parameter int NUM_NEURONS = 256,
+    //parameter NUM_WEIGHTS = NUM_INPUTS,
+    parameter int POPCOUNT_WIDTH = 32
+    //parameter int NUM_BEATS = $ceil(NUM_INPUTS / BEAT_WIDTH)
 )(
     input logic clk,
     input logic [BEAT_WIDTH-1:0] x,
-    output logic [NUM_NEURONS-1:0] out,
     input logic rst, 
     input logic en, 
     input logic valid_in,
     input logic last,
     input logic [NUM_NEURONS-1:0][BEAT_WIDTH-1:0] w,
+    output logic [NUM_NEURONS-1:0] out,
     output logic [NUM_NEURONS-1:0][POPCOUNT_WIDTH-1:0] popcount_outs,
     output logic [NUM_NEURONS-1:0] valid_outs
 
 );
+    localparam int NUM_BEATS = (NUM_INPUTS + BEAT_WIDTH - 1) / BEAT_WIDTH; // 784/16 = 49 beats
     generate
         for (genvar i = 0; i < NUM_NEURONS; i++) begin : neurons_loop
             neuron #(
